eabc281e065faf23078f91dfa9e5b1ff4701ef42
jakobtroidl <e1525071@student.tuwien.ac.at>
2019-11-08 15:02:08 +0100

    computetd error more efficiently

--- Assignment1/Part2/Jakob/part2.m
+++ Assignment1/Part2/Jakob/part2.m
@@ -1,47 +1,35 @@
 % Experimental setup
-global d;
+N = 7;
 d = 2;
+threshold = 0.2; % training will terminate when error is lower than threshold
+gamma = 0.3; % learning rate
 
 x = 0:0.1:5;
 y = 2 * x.^2 - 5 * x + 1;
 
 % extracting the training set
-
-x_training = x(1:8:end);
-t_training = y(1:8:end);
+x_training = x(1:(N+1):end);
+t = y(1:(N+1):end);
 
 % compute offsets for t_training using the normal distribution
-
 % create the normal distribution
 n_x = -5:.1:5;
 n_dist = normpdf(n_x, 0, 4);
 
 % extract 7 random values of the normal distribution
-indices = randperm(numel(n_dist), 7);
+indices = randperm(numel(n_dist), N);
 offset = n_dist(indices);
 
-t_training = t_training + offset;
+t = t + offset;
 
-w = [0 0 0];
+power = [0; 1; 2]; % powers of the phi function
+X = repmat(x_training, d + 1, 1);
 
-error = rss(w)
+% X matrix is created based on the phi function
+X = X .^ power;
 
-% compute the residual sum of squares error (RSS) for a given weight vector
-% w. the output value is the error
-function [error] = rss(w)
-    global x_training;
-    global t_training;
-    error = 0;
-    for i = 1:size(x_training, 2)
-        ti = t_training(i);
-        xi = x_training(i);
-        error = error + (ti - dot(w, phi(xi))); 
-    end
-end
+w = [0; 0; 0];
 
-function [res] = phi(x_i)
-    global d;
-    p = 0:1:d;
-    x_i = repmat(x_i, 1, d + 1);
-    res = x_i .^ p;
-end
+% compute the error 
+o = transpose(w) * X;
+error = (t - o) * (t - o)';
