3a13f01d0b5bd484d2994e21f7416994aa0de7be
jakobtroidl <e1525071@student.tuwien.ac.at>
2019-11-08 17:56:32 +0100

    implemented LMS learning rule
    not working yet

--- Assignment1/Part2/Jakob/part2.m
+++ Assignment1/Part2/Jakob/part2.m
@@ -1,8 +1,8 @@
-% Experimental setup
+%% Experimental setup
 N = 7;
 d = 2;
-threshold = 0.2; % training will terminate when error is lower than threshold
-gamma = 0.3; % learning rate
+threshold = 0.02; % training will terminate when error is lower than threshold
+gamma = 0.02; % learning rate
 
 x = 0:0.1:5;
 y = 2 * x.^2 - 5 * x + 1;
@@ -16,20 +16,54 @@ t = y(1:(N+1):end);
 n_x = -5:.1:5;
 n_dist = normpdf(n_x, 0, 4);
 
-% extract 7 random values of the normal distribution
+% extract N random values of the normal distribution
 indices = randperm(numel(n_dist), N);
 offset = n_dist(indices);
-
-t = t + offset;
 
 power = [0; 1; 2]; % powers of the phi function
 X = repmat(x_training, d + 1, 1);
 
+%% Parameter Optimization
 % X matrix is created based on the phi function
 X = X .^ power;
-
+t = t + offset;
 w = [0; 0; 0];
 
 % compute the error 
-o = transpose(w) * X;
-error = (t - o) * (t - o)';
+e = error(w, X, t);
+diff = realmax('double');
+counter = 0;
+
+% perform training
+while counter < 100
+    %i = mod(counter, N) + 1;
+   
+    for i = 1:7 
+        w = w + 2 * gamma * (t(i) - w' * X(:, i)) * X(:, i);
+    end
+    
+    new_error = error(w, X, t); 
+    
+    out = [ 'Iterations: ', num2str(counter), ...
+            ' Error: ', num2str(new_error), ... 
+            sprintf(' w: [%d, %d, %d]', w)];
+        
+    disp(out);
+    counter = counter + 1;
+    e = new_error;
+end
+
+% plot(x, y, 'Color', 'green');
+% hold on
+% scatter(x_training, t);
+% %hold on
+% %fplot(@(x) w(1) + x * w(2) + x^2 * w(3),'Color', 'red')
+% hold off
+
+
+
+function e = error(w, X, t)
+% computes the error from a given weight vector
+    o = w' * X;
+    e = (t - o) * (t - o)';
+end
