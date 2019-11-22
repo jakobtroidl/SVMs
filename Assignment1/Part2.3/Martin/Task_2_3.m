706d36327af63d8a68b014085bde4d16265249cd
Martin Ptáček <marty.mptacek@gmail.com>
2019-11-22 23:35:35 +0100

    Task 2.3: Generating png graphs. Calculation of influence of x variance to RSS of complete image sample.

--- Assignment1/Part2.3/Martin/Task_2_3.m
+++ Assignment1/Part2.3/Martin/Task_2_3.m
@@ -1,24 +1,27 @@
 clear all;
 close all;
 
-% Parameters
+%% Parameter settings
+% Calculation Parameters
 groupID = 5;
 N = 7;  % training set size
 imageSizeX = 29;
 imageSizeY = 29;
 muX = ceil(imageSizeY/2);
 muY = ceil(imageSizeX/2);
-sigma = 2;
+stDeviation = 2;    % default 2
 displayScale = 3;
 lambda = 0.1;   % ridge penalty
+x = 0:0.1:5;    % predictor
+y = 2*x.^2 -groupID.*x + 1; % target values
 
-%% Calculate w* in closed form
+% Display parameters
+lineWidth = 1;
+markerSize = 12;
 
-x = 0:0.1:5;
-y = 2*x.^2 -groupID.*x + 1;
-m1 = normrnd(muX, sigma, 1, size(x, 2));
-m2 = normrnd(muY, sigma, 1, size(x, 2));
-
+%% Calculation of influence of increasing variance to the RSS
+stDeviationArray = linspace(stDeviation^2, 10*stDeviation^2, 10).^0.5;
+errorRSS = stDeviationArray.*0;
 
 trainDataStep = (size(x, 2) - mod(size(x, 2), N))/N + 1;
 if trainDataStep == 0
@@ -29,60 +32,81 @@ xTraining = x(1:trainDataStep:trainDataStep*N);
 yTraining = y(1:trainDataStep:trainDataStep*N);
 tTraining = y(1:trainDataStep:trainDataStep*N);
 
-%Image generation
-for img = 1:size(x, 2)
-    xImages(:, :, img) = zeros(imageSizeY, imageSizeX);
-    for i = 1:imageSizeY
-        for j = 1:imageSizeX
-            if ((i - m1(img))^2 + (j - m2(img))^2 - (3*x(img))^2) < 0
-                xImages(i, j, img) = 1;
+for stDevIndex = 1:size(stDeviationArray, 2)
+    % Circle center x, y coordinates including noise
+    m1 = normrnd(muX, stDeviationArray(stDevIndex), 1, size(x, 2));
+    m2 = normrnd(muY, stDeviationArray(stDevIndex), 1, size(x, 2));
+
+    % Generate images
+    for img = 1:size(x, 2)
+        xImages(:, :, img) = zeros(imageSizeY, imageSizeX);
+        for i = 1:imageSizeY
+            for j = 1:imageSizeX
+                if ((i - m1(img))^2 + (j - m2(img))^2 - (3*x(img))^2) < 0
+                    xImages(i, j, img) = 1;
+                end
             end
         end
+        xImagesArray(:, img) = [1; reshape(xImages(:, :, img), [imageSizeX*imageSizeY, 1])];
     end
-    xImagesArray(:, img) = [1; reshape(xImages(:, :, img), [imageSizeX*imageSizeY, 1])];
+
+    xTrainImages = xImages(:, :, 1:trainDataStep:trainDataStep*N);
+    xTrainImagesArray = xImagesArray(:, 1:trainDataStep:trainDataStep*N);
+    
+    % Calculate w* in closed form
+    wEstimate = (xTrainImagesArray*xTrainImagesArray' + lambda*eye(imageSizeX*imageSizeY + 1))\xTrainImagesArray*tTraining';
+    
+    % Compute the training RSS error
+    yTrainPredicted = wEstimate'*xTrainImagesArray;
+    trainingErrorRSS = sum((yTraining - yTrainPredicted).^2)    % RSS (Residual Sum of Squares)
+    
+    % Compute complete sample RSS error
+    yPredicted = wEstimate'*xImagesArray;
+    errorRSS(stDevIndex) = sum((y - yPredicted).^2);
+    
+    if stDevIndex == 1  % plot graphs for stDev = 2
+        displayArray = [];
+        for img = 1:N
+            displayArray = [displayArray xTrainImages(:, :, img)];
+            displayArray = [displayArray ones(imageSizeY, displayScale)];
+        end
+
+        figure;
+        imshow(displayArray);
+        title([num2str(N) ' training images'], 'Interpreter', 'latex');
+        truesize([imageSizeY*displayScale, imageSizeX*displayScale*N]);
+        saveas(gcf,['Training_images.png']);
+        
+        % Plot the predicted ^y_i vs. the true y_i for the training images.
+        figure;
+        plot(x, y, 'g', 'LineWidth', lineWidth);
+        hold on;
+        plot(xTraining, yTrainPredicted, 'bx', 'LineWidth', lineWidth, 'MarkerSize', markerSize);
+        title(['Comparison of true target $y$ to ' num2str(N) ' train images $\hat{y}$ prediction'], 'Interpreter', 'latex');
+        legend('true target $y$', 'predicted $\hat{y}$', 'Interpreter', 'latex');
+        xlabel('Input x [px]', 'Interpreter', 'latex');
+        ylabel('Output $y, \hat{y}$ [-]', 'Interpreter', 'latex');
+        text(min(x), max(y), ['Training error RSS = ' num2str(trainingErrorRSS) '[-]'], 'Interpreter', 'latex');
+        grid on;
+        saveas(gcf,['Evaluation_restricted_train_images.png']);
+        
+        figure;
+        plot(x, y, 'g', 'LineWidth', lineWidth);
+        hold on;
+        plot(x, yPredicted, 'bx', 'LineWidth', lineWidth, 'MarkerSize', markerSize);
+        title(['Comparison of true target $y$ to all ' num2str(size(x, 2)) ' images $\hat{y}$ prediction'], 'Interpreter', 'latex');
+        legend('true target $y$', 'predicted $\hat{y}$', 'Interpreter', 'latex');
+        xlabel('Input x [px]', 'Interpreter', 'latex');
+        ylabel('Output $y, \hat{y}$ [-]', 'Interpreter', 'latex');
+        grid on;
+        saveas(gcf,['Evaluation_all_images.png']);
+    end
 end
 
-xTrainImages = xImages(:, :, 1:trainDataStep:trainDataStep*N);
-xTrainImagesArray = xImagesArray(:, 1:trainDataStep:trainDataStep*N);
-
-
-displayArray = [];
-for img = 1:N
-    displayArray = [displayArray xTrainImages(:, :, img)];
-    displayArray = [displayArray ones(imageSizeY, displayScale)];
-end
-
 figure;
-imshow(displayArray);
-title([num2str(N) ' training images']);
-truesize([imageSizeY*displayScale, imageSizeX*displayScale*N]);
-
-wAsterisk = (xTrainImagesArray*xTrainImagesArray' + lambda*eye(imageSizeX*imageSizeY + 1))\xTrainImagesArray*tTraining';
-
-%% Plot the predicted ?y_i vs. the true y_i for the 7 training images. Compute the training error.
-
-yTrainPredicted = wAsterisk'*xTrainImagesArray;
-trainingError = 0.5*sum((yTraining - yTrainPredicted).^2)    % SSE
-
-figure;
-plot(x, y);
-hold on;
-plot(xTraining, yTrainPredicted, 'x');
-title(['Comparison of true y to ' num2str(N) ' train images $\hat{y}$ prediction'], 'Interpreter', 'latex');
-legend('true y', 'predicted $\hat{y}$', 'Interpreter', 'latex');
-xlabel('Input x [-]', 'Interpreter', 'latex');
-ylabel('Output y, $\hat{y}$ [-]', 'Interpreter', 'latex');
-text(min(x), max(y), ['Training error SSE = ' num2str(trainingError)], 'Interpreter', 'latex');
-
-%% Plot the predicted ?yi vs. the true yi for all 51 images.
-
-yPredicted = wAsterisk'*xImagesArray;
-
-figure;
-plot(x, y);
-hold on;
-plot(x, yPredicted, 'x');
-title(['Comparison of true y to all ' num2str(size(x, 2)) ' images $\hat{y}$ prediction'], 'Interpreter', 'latex');
-legend('true y', 'predicted $\hat{y}$', 'Interpreter', 'latex');
-xlabel('Input x [-]', 'Interpreter', 'latex');
-ylabel('Output y, $\hat{y}$ [-]', 'Interpreter', 'latex');
+plot(stDeviationArray.^2, errorRSS, 'x', 'LineWidth', lineWidth, 'MarkerSize', markerSize);
+xlabel('Variance of circle center coordinates $\sigma [px^2]$', 'Interpreter', 'latex');
+ylabel('Full image sample error RSS [-]', 'Interpreter', 'latex');
+title(['Influence of circle coordinates variance to RSS of full image sample'], 'Interpreter', 'latex');
+grid on;
+saveas(gcf,['VarianceToRSS.png']);
