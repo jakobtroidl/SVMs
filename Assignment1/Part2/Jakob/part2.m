2a42e71a5d48cf0505e1d61d061b71dff09292d0
jakobtroidl <e1525071@student.tuwien.ac.at>
2019-11-23 17:52:49 +0100

    added some pictures and and error plot file

--- Assignment1/Part2/Jakob/part2.m
+++ Assignment1/Part2/Jakob/part2.m
@@ -1,13 +1,13 @@
 %% Experimental setup
 N = 7;
 d = 2;
-threshold = 80; % training will terminate when error is lower than threshold
-gamma = 0.002; % learning rate
+threshold = 50; % training will terminate when error is lower than threshold
+gamma = 0.025; % learning rate
 max_iterations = 10000; % max number of training iterations
 
 % generate data for the true target function
 x = 0:0.1:5; 
-y = 2 * x.^2 - 5 * x + 1;
+y = 2 * x.^2 - 5 .* x + 1;
 
 % extracting the training set
 x_training = x(1:(N+1):end);
@@ -16,7 +16,7 @@ t = y(1:(N+1):end);
 % compute offsets for t_training using the normal distribution
 offset = normrnd(0, 4, 1, N);
 
-power = (0:1:d)'; % powers of the phi function
+power = (d:-1:0)'; % powers of the phi function
 X = repmat(x_training, d + 1, 1);
 
 %% Parameter Optimization
@@ -26,40 +26,51 @@ t = t + offset;
 w = [0; 0; 0];
 
 % compute the error 
+e_old = realmax;
+c = realmax;
 e = error(w, X, t);
 counter = 0;
 
 % perform training
-while counter < max_iterations && e > threshold
+while counter < max_iterations && e > threshold &&  c > 0.0009
     
     % compute current index in [1, N]
     i = mod(counter, N) + 1;
     
     % update weight vector
-    w = w + 2 * gamma * (t(i) - w' * X(:, i)) * X(:, i);
-    e = error(w, X, t); 
+    w = w + 2 * (gamma * (counter + 1)^(-1/2)) * (t(i) - w' * X(:, i)) * X(:, i);
+    e = error(w, X, t);
+    
+    %check for convergence
+    c = abs(e_old - e);
+    e_old = e;
     
     % generate console output
-    out = [ 'Iterations: ', num2str(counter), ...
-            ' Error: ', num2str(e), ... 
-            sprintf(' w: [%d, %d, %d]', w) ];
-        
-    disp(out);
+%     out = [ 'Iterations: ', num2str(counter), ...
+%             ' Error: ', num2str(e), ... 
+%             sprintf(' w: [%d, %d, %d]', w) ];
+%         
+%     disp(out);
     counter = counter + 1;
 end
 
 % compute final target values
-t_out = zeros(size(t));
+t_out = zeros(1, size(x, 2));
 for i = 1:size(x,2)
     t_out(i) = w' * (repmat(x(i), d + 1, 1) .^ power);
 end
 
+
+
 % compute w* in closed version
-w_closed = pinv(X * X') * X * t';
-t_out_closed = zeros(size(t));
+w_closed = pinv(X') * t';
+t_out_closed = zeros(1, size(x, 2));
 for i = 1:size(x,2)
     t_out_closed(i) = w_closed' * (repmat(x(i), d + 1, 1) .^ power);
 end
+
+out = ['Online: ', num2str(error(w, X, t)), 'Closed: ', num2str(error(w_closed, X, t))];
+disp(out);
 
 % print results
 plot(x, y, 'Color', 'green', 'linewidth', 2);
