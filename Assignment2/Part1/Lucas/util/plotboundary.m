ad33755d62424db96dc2e5218967646f2735f110
Lucas Melo <lwcmelo@gmail.com>
2019-12-29 20:24:14 +0100

    plot boundary

---
+++ Assignment2/Part1/Lucas/util/plotboundary.m
@@ -0,0 +1,30 @@
+function plotboundary(a, w0, X, t, plotsurf)
+% a = alpha
+% Had to rename to a because we
+%   want to use the command 'alpha'
+
+if nargin < 5
+    plotsurf = false;
+end
+
+[x, y] = meshgrid(0:0.01:1);
+len = size(x,2);
+n = numel(x);
+
+Xnew = [reshape(x,n,1) reshape(y,n,1)];
+z = discriminant(a, w0, X, t, Xnew);
+
+z = reshape(z, len, len);
+
+contour(x,y,z,[0 0],'-k');
+contour(x,y,z+1,[0 0],'--k');
+contour(x,y,z-1,[0 0],'--k');
+
+if plotsurf
+    surf(x,y,z);
+    shading interp;
+    alpha 0.5;
+    view(3);
+end
+    
+end
--- Assignment2/Part1/Lucas/util/plotdata.m
+++ Assignment2/Part1/Lucas/util/plotdata.m
@@ -1,4 +1,9 @@
-function plotdata(X, t)
+function plotdata(X, t, color1, color2)
+
+if nargin <= 2
+    color1 = [1 0 0];
+    color2 = [0 1 0];
+end
 
 ax = X(t == -1, 1);
 ay = X(t == -1, 2);
@@ -6,15 +11,14 @@ bx = X(t == 1, 1);
 by = X(t == 1, 2);
 
 c = lines(2);
-scatter(ax, ay, 'xr');%, 'MarkerEdgeColor', c(1,:));
+scatter(ax, ay, 'x', 'MarkerEdgeColor', color1);
 hold on;
-scatter(bx, by, '+g');%, 'MarkerEdgeColor', c(2,:));
-%hold off;
+scatter(bx, by, '+', 'MarkerEdgeColor', color2);
 %legend('Zeros', 'Ones');
 mini = min(X);
 maxi = max(X);
-hor = (maxi(1) - mini(1)) / 4;
-ver = (maxi(2) - mini(2)) / 4;
+hor = (maxi(1) - mini(1)) / 10;
+ver = (maxi(2) - mini(2)) / 10;
 xlim([mini(1)-hor maxi(1)+hor]);
 ylim([mini(2)-ver maxi(2)+ver]);
 
