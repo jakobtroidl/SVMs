9531dde237c799440f32dcd016fda131b9db4153
Lucas Melo <lwcmelo@gmail.com>
2020-01-05 17:39:29 +0100

    changed calculation of w0

--- Assignment2/Part2/trainSVM2.m
+++ Assignment2/Part2/trainSVM2.m
@@ -25,7 +25,13 @@ ub = ones(N, 1) * C;
 
 %% Getting w0
 % Based on a support vector 's'
-[~,s] = max(alpha);
+%  Hint: Note that with C<? you have to take care about selecting a
+% support vector xs with margin |d(xs)| = 1 to calculate w0.
+
+%[~,s] = max(alpha);
+[~,i] = sort(abs(1 - abs(alpha)));
+s = i(1);
+
 ts = t(s);
 xs = X(s,:);
 w0 = ts - ((alpha .* t)' * kernel(X, xs)); % * X * xs'
