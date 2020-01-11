12712e939b0008fcd7b6bf3739b3057b422e5450
jakobtroidl <e1525071@student.tuwien.ac.at>
2020-01-11 09:13:37 +0100

    added axis descriptions

--- Assignment2/Part3.m
+++ Assignment2/Part3.m
@@ -5,7 +5,7 @@ addpath('readmnist')
 % by selecting images from the MNIST training set, where each Tk consists
 % of not more than N=70 images
 
-M = 150; %  # of training sets
+M = 3; %  # of training sets
 N = 70; %  # of images per training set
 train = cell(M,1);
 trainL = cell(M,1); % labels
@@ -37,7 +37,10 @@ labels_test_01 = (labels_test_01 * 2) - 1; % scale labels to [-1; 1]
 % testset)
 
  %% Compare average error of Linear SVM (no kernel, no slack variable) with perceptron
+tic
 [errorLinearSVM, errorLinearPerc] = evaluate(train, trainL, imgs_test_01, labels_test_01);
+toc
+
 % calculate the average error for Perc and SVM over all M trained SVMs and Perc's 
 avg_error_svm = (sum(errorLinearSVM, 3) / M) * 100;
 avg_error_perc = (sum(errorLinearPerc, 3) / M) * 100;
@@ -45,15 +48,27 @@ avg_error_perc = (sum(errorLinearPerc, 3) / M) * 100;
 % plot results
 X = categorical({'Avg error SVM','Avg error Perc'});
 Y = [avg_error_svm avg_error_perc];
-bar(X,Y, 0.4)
+
+figure, bar(X,Y, 0.4)
 title('Average error comparison of Perceptron and SVM (linear kernel, no slack variables)')
 ylabel('% of misclassified samples')
 
 %% Explore 2D parameter space of (sigma, C) of SVM and plot average errors
 
-C = [0.5 1 3 5 7 10 20 50 Inf];
-sigma = [0.5 1 2 3 5 7 10 20 30];
+% C = [0.5 1 3 5 7 10 20 50 Inf];
+% sigma = [0.5 1 2 3 5 7 10 20 30];
+C = [5 7 10];
+sigma = [3 5 7];
 
+tic
 [errorSVM, errorPerc] = evaluate(train, trainL, imgs_test_01, labels_test_01, C, sigma);
+toc 
+
 avg_error_svm = (sum(errorSVM, 3) ./ M) * 100;
-bar3(avg_error_svm)
+[minimum, idx] = min(avg_error_svm);
+figure; 
+b = bar3(avg_error_svm);
+set(gca,'XtickLabel', C);
+set(gca,'YtickLabel', sigma);
+xlabel('regularization param. C'), ylabel('sigma'), zlabel('Avg. error in %')
+title('Avg. error over the 2D parameter space')
