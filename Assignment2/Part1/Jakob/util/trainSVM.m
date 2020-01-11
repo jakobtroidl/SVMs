a3fb6bf22bd00bbabad98cfa0d1b7c51a47351da
Lucas Melo <lwcmelo@gmail.com>
2020-01-11 15:00:23 +0100

    deleted parts 1 and 2

--- Assignment2/Part1/Jakob/util/trainSVM.m
+++
@@ -1,8 +0,0 @@
-function [alpha, w0] = trainSVM(X, t)
-% Implementthefunction[alpha, w0] = trainSVM(X,t) using the quadratic optimizer (e.g., quadprog) 
-% to solve the dual optimization problem. The vector alpha holds the optimal dual parameter values, 
-% i.e., the lagrange multipliers ?i for all N input vectors 
-
-
-end
-
--- Assignment2/Part1/Lucas/discriminant.m
+++
@@ -1,8 +0,0 @@
-function [y] = discriminant(alpha,w0,X,t,Xnew)
-
-% y = d(x) = (w' * x) + w0
-% w = sum from i=1 to N of:  ai * xi * ti
-% therefore:  y = ((ai * xi * ti)' * x) + w0
-y = (((alpha .* t)' * X * Xnew') + w0)';
-
-end
