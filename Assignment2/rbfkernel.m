c160300fe2d062c5b3d4e666633bfb10108907e4
jakobtroidl <e1525071@student.tuwien.ac.at>
2020-01-11 10:08:23 +0100

    vectorized kernel function for n dimensions

--- Assignment2/rbfkernel.m
+++ Assignment2/rbfkernel.m
@@ -18,14 +18,11 @@ function [k] = rbfkernel(x1, x2, sigma)
 
     x = size(x1, 1);
     y = size(x2, 1);
-    dim = size(x1, 2);
-    components = zeros(x, y, dim);
     
-    for i = 1:dim
-        aPlus = repmat(x1(:, i), 1, y);
-        aMinus = repmat(x2(:, i)', x, 1);
-        components(:, :, i) = aPlus - aMinus;
-    end
+    aPlus = repmat(x1, [1 1 y]);
+    aMinus = repmat(x2,[1 1 x]);
+    aMinus = permute(aMinus, [3 2 1]);
+    components = permute(aPlus - aMinus, [1 3 2]);
     
     norm = sum(components .^ 2, 3);
     k = exp( - norm / (sigma ^ 2));
